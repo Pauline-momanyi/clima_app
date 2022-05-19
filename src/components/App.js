07c628d6947a51317b485ba2330433acf6920b45
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-05-20 00:40:38 +0300

    Add sign in and sign up routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,26 @@
 import "../App.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router , Switch, Route} from "react-router-dom";
 import Nav from "./layout/Nav";
 import Try from "./Try";
 import Header from "./dashboard/Header";
 import Categories from "./dashboard/Categories";
+import SignIn from "./auth/SignIn";
+import SignUp from "./auth/SignUp"
 
 function App() {
   return (
     <Router>
       <div className="sm:mx-10">
         <Nav />
-        <div className="flex mt-2 mx-2">
+        {/* <div className="flex mt-2 mx-2">
           <Categories />
           <Header />
-        </div>
+        </div> */}
       </div>
+      <Switch>
+        <Route exact path='/signin'><SignIn/></Route>
+        <Route exact path='/signup'><SignUp/></Route>
+      </Switch>
     </Router>
   );
 }
