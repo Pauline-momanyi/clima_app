1ad330f3954d7df4949942e8796b94cbbe13fc52
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-05-26 14:00:31 +0300

    Add categories based on tags

--- src/components/dashboard/Categories.js
+++ src/components/dashboard/Categories.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
-function Categories() {
+function Categories({data}) {
+  // console.log(data);
+  const tagArray = data.map(data=> data.tags)
+  const tags = tagArray.map(tag=>tag)
+  console.log(tags);
+  
+
   return (
-    <div className='border border-orange-primary mr-4 h-48'>
-        <h2>Categories</h2>
+    <div className='border border-orange-primary mr-4 p-3 h-full'>
+        <h2 className='font-semibold'>Popular Tags</h2>      
+        
+          {tags.map(tag=>(
+            <ul>{tag.map(itag=>(
+              <li>{itag}</li>
+              ))}
+            </ul>
+          
+          )
+          )}          
+         
+        
     </div>
   )
 }
